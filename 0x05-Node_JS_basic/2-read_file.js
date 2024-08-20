5bd68bb24e22106f99959cefb253bee195c4974c
meta-bay <bayiemeseret@gmail.com>
2024-08-20 23:55:06 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,40 @@
-const fs = require("node:fs");
+const fs = require('node:fs')
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, "utf-8").toString();
-    if (!data) {
-      throw new Error("Cannot load the database");
+    const data = fs.readFileSync(path, "utf8");
+    const studentsList = data.split("\n").slice(1, -1);
+    const dbList = [];
+    const fields = {};
+    for (const i of studentsList) {
+      const temp = i.split(",");
+      dbList.push(temp);
+      fields[temp[temp.length - 1]] = [];
+    }
+    for (const i of dbList) {
+      if (i[i.length - 1] === "CS") fields.CS.push(i[0]);
+      else if (i[i.length - 1] === "SWE") fields.SWE.push(i[0]);
     }
 
-    let countCs = 0;
-    let countSwe = 0;
-    let sweStudents = "";
-    let csStudents = "";
+    const info = {
+      count: studentsList.length,
+      cs: {
+        count: fields.CS.length,
+        names: fields.CS.join(", "),
+      },
+      swe: {
+        count: fields.SWE.length,
+        names: fields.SWE.join(", "),
+      },
+    };
 
-    const rows = data.split("\n");
-    rows.forEach((row) => {
-      const columns = row.split(",");
-      if (columns.length >= 4) {
-        if (columns[3] === "CS") {
-          countCs += 1;
-          csStudents += (csStudents ? ", " : "") + columns[0];
-        }
-        if (columns[3] === "SWE") {
-          countSwe += 1;
-          sweStudents += (sweStudents ? ", " : "") + columns[0];
-        }
-      }
-    });
-
-    const totalStudents = countCs + countSwe;
-    console.log(`Number of students: ${totalStudents}`);
-    console.log(`Number of students in CS: ${countCs}. ${csStudents}`);
-    console.log(`Number of students in SWE: ${countSwe}. ${sweStudents}`);
+    console.log(`Number of students: ${info.count}`);
+    console.log(
+      `Number of students in CS: ${info.cs.count}. List: ${info.cs.names}`
+    );
+    console.log(
+      `Number of students in SWE: ${info.swe.count}. List: ${info.swe.names}`
+    );
   } catch (err) {
     throw new Error("Cannot load the database");
   }
