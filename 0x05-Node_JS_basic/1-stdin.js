aa298b4d17665978a1e87cccb9208e6a5e55bc6a
meta-bay <bayiemeseret@gmail.com>
2024-08-20 18:14:44 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.setEncoding("utf-8");
-process.stdin.on("readable", () => {
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', () => {
   const user_name = process.stdin.read();
   if (user_name) {
     process.stdout.write(`Your name is: ${user_name}`);
   }
 });
 
-process.stdin.on("end", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
