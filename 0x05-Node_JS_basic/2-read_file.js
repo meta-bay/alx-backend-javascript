23a863da910a8b7bef50ad6666dfdddabda9bd97
meta-bay <bayiemeseret@gmail.com>
2024-08-21 00:13:44 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,37 +3,32 @@ const fs = require("node:fs");
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, "utf8");
-    const studentsList = data.split("\n").slice(1);
-    const dbList = [];
-    const fields = {};
-    for (const i of studentsList) {
-      const temp = i.split(",");
-      dbList.push(temp);
-      fields[temp[temp.length - 1]] = [];
-    }
-    for (const i of dbList) {
-      if (i[i.length - 1] === "CS") fields.CS.push(i[0]);
-      else if (i[i.length - 1] === "SWE") fields.SWE.push(i[0]);
-    }
+    const studentsList = data.split("\n").filter((line) => line.trim() !== "");
+    studentsList.shift();
 
-    const info = {
-      count: studentsList.length,
-      cs: {
-        count: fields.CS.length,
-        names: fields.CS.join(", "),
-      },
-      swe: {
-        count: fields.SWE.length,
-        names: fields.SWE.join(", "),
-      },
-    };
+    const fields = { CS: [], SWE: [] };
 
-    console.log(`Number of students: ${info.count}`);
+    studentsList.forEach((line) => {
+      const [firstname, , , field] = line.split(",");
+      if (field === "CS") {
+        fields.CS.push(firstname);
+      } else if (field === "SWE") {
+        fields.SWE.push(firstname);
+      }
+    });
+
+    const totalStudents = fields.CS.length + fields.SWE.length;
+
+    console.log(`Number of students: ${totalStudents}`);
     console.log(
-      `Number of students in CS: ${info.cs.count}. List: ${info.cs.names}`
+      `Number of students in CS: ${fields.CS.length}. List: ${fields.CS.join(
+        ", "
+      )}`
     );
     console.log(
-      `Number of students in SWE: ${info.swe.count}. List: ${info.swe.names}`
+      `Number of students in SWE: ${fields.SWE.length}. List: ${fields.SWE.join(
+        ", "
+      )}`
     );
   } catch (err) {
     throw new Error("Cannot load the database");
