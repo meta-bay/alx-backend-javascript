c2c358c74d06087ef9150b31b6b98e23e2aacefc
meta-bay <bayiemeseret@gmail.com>
2024-08-20 17:56:01 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.setEncoding("utf-8");
 process.stdin.on("readable", () => {
   const user_name = process.stdin.read();
   if (user_name !== null) {
-    process.stdout.write("Your name is: " + user_name);
+    process.stdout.write(`Your name is: ${user_name}`);
   }
 });
 
