4de65824e0f8b35d7a85aa0aa9b7f19e6b569d16
meta-bay <bayiemeseret@gmail.com>
2024-08-20 22:13:28 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,39 @@
 const fs = require("node:fs");
 
 function countStudents(path) {
-  var fs = require("fs");
   try {
-    var data = fs.readFileSync(path).toLocaleString();
+    const data = fs.readFileSync(path, "utf-8").toString();
     if (!data) {
       throw new Error("Cannot load the database");
     }
+
+    let countCs = 0;
+    let countSwe = 0;
+    let sweStudents = "";
+    let csStudents = "";
+
+    const rows = data.split("\n");
+    rows.forEach((row) => {
+      const columns = row.split(",");
+      if (columns.length >= 4) {
+        if (columns[3] === "CS") {
+          countCs += 1;
+          csStudents += (csStudents ? ", " : "") + columns[0];
+        }
+        if (columns[3] === "SWE") {
+          countSwe += 1;
+          sweStudents += (sweStudents ? ", " : "") + columns[0];
+        }
+      }
+    });
+
+    const totalStudents = countCs + countSwe;
+    console.log(`Number of students: ${totalStudents}`);
+    console.log(`Number of students in CS: ${countCs}. ${csStudents}`);
+    console.log(`Number of students in SWE: ${countSwe}. ${sweStudents}`);
   } catch (err) {
     throw new Error("Cannot load the database");
   }
-  countCs = 0;
-  countSwe = 0;
-  sweStudents = "";
-  csStudents = "";
-
-  var rows = data.split("\n");
-  rows.forEach((row) => {
-    columns = row.split(",");
-    if (columns[3] === "CS") {
-      countCs += 1;
-      csStudents += (csStudents ? ", " : "") + columns[0];
-    }
-    if (columns[3] === "SWE") {
-      countSwe += 1;
-      sweStudents += (sweStudents ? ", " : "") + columns[0];
-    }
-  });
-  totalStudents = countCs + countSwe;
-  console.log(`Number of students: ${totalStudents}`);
-  console.log(`Number of students in CS: ${countCs}. ${csStudents}`);
-  console.log(`Number of students in CS: ${countSwe}. ${sweStudents}`);
 }
+
 module.exports = countStudents;
