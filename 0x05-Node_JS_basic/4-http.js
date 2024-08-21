066e700d1c8d162b6cac1fa46935ce5e402f6112
meta-bay <bayiemeseret@gmail.com>
2024-08-21 04:29:31 +0300

    Create a small HTTP server using Nodes

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+server.listen(1245, '127.0.0.1');
+
+module.exports = server;
