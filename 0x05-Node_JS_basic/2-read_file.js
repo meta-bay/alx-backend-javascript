40151784a1cb57fea5950e96b03993d719de7616
meta-bay <bayiemeseret@gmail.com>
2024-08-21 04:16:37 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,32 +1,36 @@
 const fs = require('fs');
 
-function countStudents (filePath) {
+function countStudents(dbName) {
   try {
-    let lines = fs.readFileSync(filePath, 'utf-8').split('\n');
-    lines = lines.slice(1, lines.length - 1);
+    const data = fs.readFileSync(dbName, 'utf8');
+    const stdList = data.split('\n').slice(1, -1);
+    const ldb = [];
+    const fields = {};
+    for (const l of stdList) {
+      const temp = l.split(',');
+      ldb.push(temp);
+      fields[temp[temp.length - 1]] = [];
+    }
+    for (const s of ldb) {
+      if (s[s.length - 1] === 'CS') fields.CS.push(s[0]);
+      else if (s[s.length - 1] === 'SWE') fields.SWE.push(s[0]);
+    }
 
-    const fieldMap = new Map();
+    const info = {
+      count: stdList.length,
+      cs: {
+        count: fields.CS.length,
+        names: fields.CS.join(', '),
+      },
+      swe: {
+        count: fields.SWE.length,
+        names: fields.SWE.join(', '),
+      },
+    };
 
-    lines.forEach((line) => {
-      const [name, , , field] = line.split(',');
-
-      if (fieldMap.has(field)) {
-        fieldMap.get(field).names.push(name);
-        fieldMap.get(field).count += 1;
-      } else {
-        fieldMap.set(field, { names: [name], count: 1 });
-      }
-    });
-
-    console.log(`Number of students: ${lines.length}`);
-
-    fieldMap.forEach((data, field) => {
-      console.log(
-        `Number of students in ${field}: ${data.count}. List: ${data.names.join(
-          ', '
-        )}`
-      );
-    });
+    console.log(`Number of students: ${info.count}`);
+    console.log(`Number of students in CS: ${info.cs.count}. List: ${info.cs.names}`);
+    console.log(`Number of students in SWE: ${info.swe.count}. List: ${info.swe.names}`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
