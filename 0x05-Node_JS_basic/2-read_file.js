03778e46985dcc66e969bdf3d0bc9d219691ce2e
meta-bay <bayiemeseret@gmail.com>
2024-08-21 04:23:13 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,32 +1,38 @@
 const fs = require('fs');
 
-function countStudents (filePath) {
+function countStudents(filePath) {
   try {
-    let lines = fs.readFileSync(filePath, 'utf-8').split('\n');
-    lines = lines.slice(1, lines.length - 1);
+    const content = fs.readFileSync(filePath, 'utf8');
+    const students = content.split('\n').slice(1, -1);
+    const records = [];
+    const courses = {};
 
-    const fieldMap = new Map();
+    for (const student of students) {
+      const details = student.split(',');
+      records.push(details);
+      courses[details[details.length - 1]] = [];
+    }
 
-    lines.forEach((line) => {
-      const [name, , , field] = line.split(',');
+    for (const record of records) {
+      if (record[record.length - 1] === 'CS') courses.CS.push(record[0]);
+      else if (record[record.length - 1] === 'SWE') courses.SWE.push(record[0]);
+    }
 
-      if (fieldMap.has(field)) {
-        fieldMap.get(field).names.push(name);
-        fieldMap.get(field).count += 1;
-      } else {
-        fieldMap.set(field, { names: [name], count: 1 });
-      }
-    });
+    const summary = {
+      total: students.length,
+      cs: {
+        count: courses.CS.length,
+        names: courses.CS.join(', '),
+      },
+      swe: {
+        count: courses.SWE.length,
+        names: courses.SWE.join(', '),
+      },
+    };
 
-    console.log(`Number of students: ${lines.length}`);
-
-    fieldMap.forEach((data, field) => {
-      console.log(
-        `Number of students in ${field}: ${data.count}. List: ${data.names.join(
-          ', '
-        )}`
-      );
-    });
+    console.log(`Number of students: ${summary.total}`);
+    console.log(`Number of students in CS: ${summary.cs.count}. List: ${summary.cs.names}`);
+    console.log(`Number of students in SWE: ${summary.swe.count}. List: ${summary.swe.names}`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
