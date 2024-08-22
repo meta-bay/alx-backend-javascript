cb539e10374e2f7bfc21bff1a694b8aa3bab0a58
meta-bay <bayiemeseret@gmail.com>
2024-08-22 12:15:40 +0300

    what about now

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return 8 for inputs 3 and 5', () => {
