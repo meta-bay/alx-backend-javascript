35f2e423fc17abc09acbbef1821bce9e738e9bee
meta-bay <bayiemeseret@gmail.com>
2024-08-22 12:03:19 +0300

    what about now

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,31 +1,31 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
-describe('calculateNumber', function () {
-  it('...', function () {
+describe('calculateNumber', () => {
+  it('...', () => {
     assert.equal(calculateNumber(3, 5), 8);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(0.1, 1), 1);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(0, 0), 0);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(-4, 2), -2);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(1.0, 2.0), 3);
   });
-  it('...', function () {
+  it('...', () => {
     assert.equal(calculateNumber(2.234, 6.6958), 9);
   });
 });
