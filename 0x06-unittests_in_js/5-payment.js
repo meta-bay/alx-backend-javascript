81270e99f70841867d263d48622437d31d7034bb
meta-bay <bayiemeseret@gmail.com>
2024-08-23 04:56:35 +0300

    spy testing

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -3,3 +3,5 @@ const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${sum}`);
 };
+
+module.exports = sendPaymentRequestToApi;
