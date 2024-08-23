2237c7425cdbf383f0da0a83c8e2fa02f499b94f
meta-bay <bayiemeseret@gmail.com>
2024-08-23 05:19:06 +0300

    api testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -5,7 +5,7 @@ describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
 
   it('should return 200 and welcome message for GET /', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
+    request.get(`${API_URL}/`, (err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
@@ -13,7 +13,7 @@ describe('API integration test', () => {
   });
 
   it('should return 200 and correct message for valid cart ID in GET /cart/:id', (done) => {
-    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+    request.get(`${API_URL}/cart/47`, (err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 47');
       done();
@@ -21,7 +21,7 @@ describe('API integration test', () => {
   });
 
   it('should return 404 for negative cart ID in GET /cart/:id', (done) => {
-    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+    request.get(`${API_URL}/cart/-47`, (err, res, body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
